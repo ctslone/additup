ef8fae8e1e84612beec97450264f5102f62f4cf2
Charlie <ctslone27@gmail.com>
2020-07-26 18:51:12 -0400

    adding very basic homepage to just test out react and then create an API request. Using bootstrap for design and components.  Still unsure if react is the way to go.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Wrapper from './Components/Wrapper';
+import Container from './Components/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+
+  };
+
+  render() {
+    return(
+      <Wrapper>
+        <Container>
+          <div className="row text-center">
+            <div className="col-12">
+              <h1>App Title</h1>
+            </div>
+          </div>
+
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">2 + 2 = ?</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" autoFocus></input>
+            </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </Container>
+      </Wrapper>
+    )
+  }
 }
 
 export default App;
