d74bc9f5acd7ffc716eb0441d0b809ee3f17fa17
Charlie <ctslone27@gmail.com>
2020-09-21 18:34:15 -0400

    edditing owl img.

--- src/Components/Title/index.js
+++ src/Components/Title/index.js
@@ -4,7 +4,7 @@ function Title(props) {
     return (
         <div className="row text-center">
             <div className="col-12">
-                <h1 id="mainTitle"><img id="app-logo" alt="owl-log" src="owl.png"></img>Additup!</h1>
+                <h1 id="mainTitle"><img id="app-logo" alt="owl-logo" src="owl.png"></img>Additup!</h1>
             </div>
         </div>
     )
